373d2d27e8dd056631f1e8e38aa911b20bdfe6f2
Talal-Amjad <mtakamboh@gmail.com>
2023-11-19 01:49:37 +0500

    Branch manager dashboard layout added

---
+++ frontend/ShopLink/src/components/Headers/Header.jsx
@@ -0,0 +1,67 @@
+import { FaSearch, FaFilter } from 'react-icons/fa';
+import PropTypes from "prop-types";
+const Header = ({ toggleSidebar }) => {
+ 
+  return (
+    <header className="bg-white fixed w-full h-18 flex justify-between items-center transition-all duration-[400ms] z-40 p-4 dark:bg-gray-900 text-gray-400">
+      <div className="hidden logo ml-5 font-bold text-2xl text-primary md:block">
+        S<span className="ml-1 tracking-tighter">H</span>
+        <span className="ml-1 tracking-tighter">O</span>
+        <span className="ml-1 tracking-tighter">P</span>
+        <span className="ml-1 tracking-tighter">L</span>
+        <span className="ml-1 tracking-tighter">I</span>
+        <span className="ml-1 tracking-tighter">N</span>
+        <span className="ml-1 tracking-tighter">K</span>
+      </div>
+      <svg
+        className="block cursor-pointer md:hidden stroke-[#333333] dark:stroke-gray-400"
+        width="31"
+        height="20"
+        viewBox="0 0 21 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        onClick={toggleSidebar}
+      >
+        <path d="M1 1H19" strokeWidth="2" strokeLinecap="round" />
+        <path
+          d="M7.49219 6.76953H19.0022"
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
+
+        <path
+          className="stroke-[#007684]"
+          d="M1 6.76953H3.99"
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
+        <path d="M1 12.5391H19" strokeWidth="2" strokeLinecap="round" />
+      </svg>
+      <div className="header-right flex items-center mr-2 md:mr-8">
+        <div className="search flex items-center mr-2 px-2 py-1 rounded-md bg-gray-300">
+          <FaSearch className="search-icon text-gray-500" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="hidden border-none px-2 outline-none bg-gray-300 md:block"
+          />
+        </div>
+        <div className="filter flex items-center ml-2 font-sans text-black dark:text-gray-400">
+          <span className="filter-icon mr-1">
+            <FaFilter className="filter-icon-inner" />
+          </span>
+          <button className="hidden md:flex items-center bg-none border-none cursor-pointer">
+            Filter
+          </button>
+        </div>
+      </div>
+    
+    </header>
+  );
+};
+
+Header.propTypes = {
+  toggleSidebar: PropTypes.func,
+};
+
+export default Header;
---
+++ frontend/ShopLink/src/components/layouts/BranchManager/managerDashboardLayout.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import Header from "../../Headers/Header";
+import useSidebar from "../../../hooks/useSidebar";
+import Sidebar from "../../sidebar/sideBar";
+
+const DashboardLayout = ({ children }) => {
+  const [sidebarVisible, toggleSidebar] = useSidebar();
+  return (
+    <div className="min-h-screen max-w-screen overflow-x-hidden">
+      <Header toggleSidebar={toggleSidebar} />
+      <Sidebar sidebarVisible={sidebarVisible} />
+      <main className="pt-20 px-4 min-h-screen w-screen ml-0 md:ml-[20%] md:px-10 md:pt-20 md:pb-10 bg-background-gray dark:bg-gray-700 scrollbar-hide md:w-[80vw]">
+        <div className="">{children}</div>
+      </main>
+    </div>
+  );
+};
+
+DashboardLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default DashboardLayout;
