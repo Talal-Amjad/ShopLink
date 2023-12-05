cbfcedc5a94b724d4d051091003beee9c29cc74a
TalhaShahid0 <smuhammadtalha31@gmail.com>
2023-12-05 11:27:33 +0500

    Added Forgot password API

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authController = require('../controllers/authController');
 const JobApplicationController=require('../controllers/jobApplicationController');
 const postJobController=require('../controllers/postJobController');
+const forgetpass = require("../controllers/forgotPasswordController")
 
 const { authenticateToken } = require('../middleware/authMiddleware');
 
@@ -12,6 +13,9 @@ router.post('/verify', authController.verifyUser)
 router.post('/signin', authController.signIn);
 router.post('/apply',JobApplicationController.JobApplicationfunction);
 router.post('/postjob',postJobController.postJobControllers);
+router.post('/verif_foretpass_email', forgetpass.verify_forget_Password_email)
+router.post('/verify_email_encoded_pass', forgetpass.verify_forgetpass)
+router.post('/changepass', forgetpass.change_password)
 router.get('/protected', authenticateToken, (req, res) => {
   res.json({ message: 'This is a protected route', user: req.user });
 });
